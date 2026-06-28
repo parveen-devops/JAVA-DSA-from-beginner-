c29be54c1b052dceb86495c51a4a0460d93fe7a9
parveen798836-oss <parveen798836@gmail.com>
2026-06-28 12:47:50 +0530

    Initial cpmmit - JAVA DSA basics

---
+++ question2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == b) {
+            System.out.println("Equal");
+        } else {
+            if (a > b) {
+                System.out.println("A is greater");
+            } else {
+                System.out.println("A is lesser");
+            }
+        }
+
+    }
+
+}
---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int Sum = a + b;
+    System.out.println("Sum = " + Sum);
+
+  }
+}
